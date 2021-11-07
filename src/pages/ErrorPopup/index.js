7d50f292a70c86eaadc5f64763618d003edbb1f9
BrijeshMakwana18 <bamakwana2000@gmail.com>
2021-11-07 14:38:02 +0530

    animated alert ui completed

--- src/pages/ErrorPopup/index.js
+++ src/pages/ErrorPopup/index.js
@@ -1,6 +1,30 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+  Animated,
+  Easing,
+} from 'react-native';
+import {perfectSize} from '../../theme';
 import styles from './styles';
 export default function ErrorPopup() {
-  return <View style={styles.container}></View>;
+  const errorModalTop = useRef(new Animated.Value(perfectSize(-500))).current;
+  const showError = () => {};
+  return (
+    <View style={styles.container}>
+      <StatusBar translucent barStyle="light-content" />
+      <TouchableOpacity
+        onPress={() => showError()}
+        style={styles.buttonContainer}>
+        <Text style={styles.buttonTitle}>{'Alert'}</Text>
+      </TouchableOpacity>
+      <Animated.View style={[styles.errorContainer, {top: errorModalTop}]}>
+        <Text style={styles.errorText}>
+          {'Invalid credentials. Please enter email and password'}
+        </Text>
+      </Animated.View>
+    </View>
+  );
 }
