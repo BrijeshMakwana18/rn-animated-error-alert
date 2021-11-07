8d24069a74f92d2a853cc9f30f15a78a1ae3afd4
BrijeshMakwana18 <bamakwana2000@gmail.com>
2021-11-07 14:38:22 +0530

    animation for alert added

--- src/pages/ErrorPopup/index.js
+++ src/pages/ErrorPopup/index.js
@@ -11,7 +11,21 @@ import {perfectSize} from '../../theme';
 import styles from './styles';
 export default function ErrorPopup() {
   const errorModalTop = useRef(new Animated.Value(perfectSize(-500))).current;
-  const showError = () => {};
+  const showError = () => {
+    Animated.timing(errorModalTop, {
+      toValue: perfectSize(50),
+      duration: 1000,
+      useNativeDriver: false,
+      easing: Easing.elastic(1),
+    }).start();
+    setTimeout(() => {
+      Animated.timing(errorModalTop, {
+        toValue: perfectSize(-500),
+        duration: 1000,
+        useNativeDriver: false,
+      }).start();
+    }, 2000);
+  };
   return (
     <View style={styles.container}>
       <StatusBar translucent barStyle="light-content" />
